6dff18540d6bc7c9d451e06cd72c780731b9fd8e
ylluberes-dev <ylluberes19@gmail.com>
2021-05-07 15:37:30 -0800

    Initial commit

---
+++ src/main/java/com/ylluberes/dofuscollectorapi/DofusCollectorApiApplication.java
@@ -0,0 +1,13 @@
+package com.ylluberes.dofuscollectorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DofusCollectorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DofusCollectorApiApplication.class, args);
+	}
+
+}
